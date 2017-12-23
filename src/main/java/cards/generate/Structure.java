be8ff68144aa4e8e290b31b90b6fc88b24b4d457
Jurjen Stellingwerff <jstellin@betterbe.com>
2017-12-23 22:43:51 +0100

    Add dungeon tables.

--- src/main/java/cards/generate/Structure.java
+++ src/main/java/cards/generate/Structure.java
@@ -11,16 +11,21 @@ public class Structure {
 
 		Record statistic = project.table("Statistic");
 		statistic.field("name", Type.STRING);
+		statistic.field("nr", Type.INTEGER);
 		statistic.index("statisticName", "name");
-		// STRENGTH, TINKER, SOCIAL, PERCEPTION, SPEED
-		// POINT_BLANK, SHORT, LONG
+		statistic.index("statisticNr", "nr");
+		// MIGHT, TINKER, SOCIAL, PERCEPTION, SPEED
+		// POINT_BLANK, SHORT, LONG, AREA
 		// SMOKE, COVER, FULL, FLANKED
 		// DODGE, RECOIL
 		// DAMAGE, PIERCE, ARMOR
 		// ALERTNESS, PANIC, IMPRESS, RECOVER
-		// CONCENTRATE, MOVE
+		// CONCENTRATE, MOVE, FLY
 		// SLOW, BACK, SHOVE, PRONE, PIN
-		// BLIND, BURN, BLEED, KNOCK, CHOKE
+		// BLIND, BURN, BLEED, KNOCK, CHOKE, UNARM
+
+		// LEFT, RIGHT, FORWARDS, BACKWARDS
+		// NUMBER
 
 		Record cardStatistic = project.table("CardStatistic");
 		cardStatistic.field("statistic", Type.RELATION, statistic);
@@ -43,6 +48,40 @@ public class Structure {
 		rules.field("races", race, "name");
 		rules.field("cards", card, "name");
 		rules.index("rulesName", "name");
+
+		Record cards = project.table("Cards");
+		cards.field("card", Type.RELATION, card);
+
+		Record connect = project.table("Connect");
+		connect.field("type", Type.ENUMERATE).setValues("DOOR", "CLIMB", "LINE");
+		connect.field("checks", Type.ARRAY, cards);
+
+		Record room = project.table("Room");
+		room.field("name", Type.STRING);
+		room.field("opponent", Type.ARRAY, cards);
+		room.field("items", Type.ARRAY, cards);
+		room.field("connection", connect, "connect.nr");
+		connect.field("to", Type.RELATION, room);
+
+		Record goal = project.table("Goal");
+		goal.field("name,", Type.STRING);
+		goal.field("type", Type.ENUMERATE).setValues("KNOWLEDGE", "WEAPON", "WEARABLE", "STATUS", "IMPLANT");
+		goal.field("XP", Type.INTEGER);
+		goal.field("gained", Type.ENUMERATE).setValues("STASH", "OVERHEAR", "REWARD");
+
+		Record area = project.table("Area");
+		area.field("name", Type.STRING);
+		area.field("rooms", room, "nr");
+		area.field("encounter", Type.ARRAY, cards);
+		area.field("goal", goal, "goal.name");
+
+		Record skill = project.table("Skill");
+		skill.field("card", Type.RELATION, card);
+		skill.field("state", Type.ENUMERATE).setValues("STASHED", "PARTY", "ACTIVE");
+
+		Record character = project.table("Character");
+		character.field("name", Type.STRING);
+		character.field("skills", skill, "card.name");
 
 		Record game = project.table("Game");
 		game.field("name", Type.STRING);
