0629d0e3b5a86f1ae626584810bbf6a5c594baf9
Jurjen Stellingwerff <jstellin@betterbe.com>
2018-01-12 11:32:17 +0100

    Initial jetty setup. Static/dynamic content and web-sockets.

---
+++ src/main/java/cards/web/Setup.java
@@ -0,0 +1,35 @@
+package cards.web;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+public class Setup {
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		ResourceHandler resource_handler = new ResourceHandler();
+		resource_handler.setDirectoriesListed(false);
+		resource_handler.setWelcomeFiles(new String[] { "index.html" });
+		resource_handler.setResourceBase(".");
+
+		WebSocketHandler wsHandler = new WebSocketHandler() {
+			@Override
+			public void configure(WebSocketServletFactory factory) {
+				factory.register(MyEchoSocket.class);
+			}
+		};
+		ServletHandler servletHandler = new ServletHandler();
+		servletHandler.addServletWithMapping(CardsServlet.class, "/*");
+
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers(new Handler[] { resource_handler, servletHandler, wsHandler, new DefaultHandler() });
+		server.setHandler(handlers);
+		server.start();
+		server.join();
+	}
+}
